e0277c20225a1cdd935d02ec64adbb9659647ecf
Dave <dnbennett@hotmail.co.uk>
2016-10-07 16:46:02 +0100

    Changed listening port number

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ var express = require('express');
 var moment = require('moment');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 var result = {"unix": null, "natural": null};
 
 app.use('/', express.static(__dirname + '/public'));
 
 app.get('/:dateString', function (req, res) {
   var path = req.params.dateString;
-  
+
   var d = new Date(path);
-  
+
   if (/^[0-9]*$/.test(path)) {
     result.unix = path;
     d = new Date(path * 1000);
@@ -22,10 +24,10 @@ app.get('/:dateString', function (req, res) {
     result.natural = moment(d).format("MMMM DD, YYYY");
     result.unix = d.getTime() / 1000;
   }
-  
+
   res.send(result);
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(port, function () {
+  console.log('Example app listening on port' + port);
 });
